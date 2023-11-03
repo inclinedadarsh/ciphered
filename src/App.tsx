a6ade639c273dab6cf51bb7d455045e094536eee
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-03 23:54:23 +0530

    feat: :sparkles: add dark mode to the website
    add dark mode theme provider. also add a toggle button to switch between modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <h1>Hello There!</h1>
+      <ModeToggle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
