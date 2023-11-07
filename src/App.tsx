7a11c354dfb1f04e3a0de492c9a70cf65dae4c49
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-07 19:50:44 +0530

    feat: :sparkles: add react-router-dom and routing for home page
    installed react-router-dom, and add routing for Main on route "/"

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Navbar />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { Toaster } from "@/components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>
 );
